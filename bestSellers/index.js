083a12ff6462a34e1774c84905d497d6382b4f75
Noam Rom <rom.noam89@gmail.com>
2015-05-12 08:56:09 +0300

    final

---
+++ bestSellers/index.js
@@ -0,0 +1,37 @@
+//JSON object that contain best seller books
+var BSArray = [
+    {"Name" : "Ronaldo","year" : "1989","BSMonth" : 'March'},
+    {"Name" : "Messi","year" : "2005","BSMonth" : 'May'},
+    {"Name" : "Gerrard","year" : "2010","BSMonth" : 'June'},
+    {"Name" : "Zidan","year" : "2011","BSMonth" : 'June'},
+    {"Name" : "Cafu","year" : "2012","BSMonth" : 'September'},
+    {"Name" : "Xavi","year" : "2013","BSMonth" : 'July'},
+    {"Name" : "Pep","year" : "2015","BSMonth" : 'January'}
+];
+
+//Returning all the best seller books
+exports.getAllBestSellersBooks = function(){
+    return BSArray;
+};
+
+//Returning only 1 best seller book by specific name(id)
+exports.getBookName = function(id){
+   for(var i = 0; i < BSArray.length; i++){
+        if(BSArray[i].Name == id){
+            return BSArray[i];
+        }
+   }
+};
+
+//Returning an array of all best seller books by specific month
+exports.getBSForMonth = function(month){
+  var newJson = {'BestSellerForMonth':[]};
+    for(var i = 0; i < BSArray.length; i++){
+        if(BSArray[i].BSMonth == month){
+            newJson.BestSellerForMonth.push(BSArray[i]);
+        }
+    }
+    return newJson;
+};
+
+
---
+++ server.js
@@ -0,0 +1,40 @@
+var express = require("express");
+var bestSellers = require('./bestSellers');
+var app = express();
+app.listen(8080);
+console.log("listening on port 8080......waiting for requests");
+
+var books = bestSellers.getAllBestSellersBooks();// Return a Json object with all of the best seller books
+
+// Displaying all the best seller books on the client site
+app.get('/',function(req,res){
+    res.status(200);
+    res.json(books);
+    console.log('---- Response done? ' + res.finished);
+    console.log('\n---- Header sent: ' + res._header);
+});
+
+//GET for specific book, by name
+app.get('/name/:bookid',function(req,res){
+    console.log('Showing result for book ID: ' + req.params.bookid + '\n');
+});
+
+app.param('bookid',function(req,res,next,value){
+    var  bookId = bestSellers.getBookName(value);
+    console.log(bookId);
+    res.json(bookId);
+    next();
+});
+
+//GET for Json object of specific books by month
+app.get('/BSForMonth/:bookmonth',function(req,res){
+    console.log('Showing result for best seller books by month: ' + req.params.bookmonth + '\n');
+});
+
+app.param('bookmonth',function(req,res,next,value){
+    var  BSForMonth = bestSellers.getBSForMonth(value);
+    console.log(BSForMonth);
+    res.json(BSForMonth);
+    next();
+});
+
